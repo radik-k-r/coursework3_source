106a467b902705f4f08421a8686a77233a3966c1
radik-k-r <107826050+radik-k-r@users.noreply.github.com>
2022-08-20 17:40:17 +0500

    Перед консультацией

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
 from flask import Flask, render_template
-from utils import get_all, get_bookmarks
+from utils import get_posts_all, get_bookmarks, get_post_by_pk, get_comments, get_comments_by_post_id, get_posts_by_user
 
 app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/index")
-def index():
-    data = get_all()
+def homepage():
+    posts = get_posts_all()
     bookmarks = get_bookmarks()
-    return render_template("index.html", data=data, bookmarks=bookmarks)
+    return render_template("index.html", posts=posts, bookmarks=bookmarks)
 
 
 @app.route("/post/<user_name>")
 def show_posts(user_name):
-    return render_template("post.html", user_name=user_name)
+    users_post = get_posts_by_user(user_name)
+    return render_template("post.html", user_name=user_name, users_post=users_post)
 
 
 @app.route("/bookmarks")
 def show_bookmarks():
     return render_template("bookmarks.html")
+
+
+@app.route("/user-feed")
+def show_feeds():
+    return render_template("user-feed.html")
+
+
+@app.route("/posts/<int:pk>")
+def show_post(pk):
+    post = get_post_by_pk(pk)
+    comments = get_comments_by_post_id(pk)
+    return render_template("post.html", post=post, comments=comments)
+
+
+@app.route("/search")
+def search_posts():
+    return render_template("search.html")
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,20 +1,28 @@
 import json
 
 
-def get_all():
-    """Возвращает посты"""
+def get_posts_all() -> object:
+    """Возвращает список словарей со вложенными постами"""
     with open("data/data.json", "r", encoding="utf-8") as file:
         return json.loads(file.read())
 
-def get_comments_from_json():
-    pass
+
+def get_comments_all() -> object:
+    """Возвращает список словарей со вложенными комментариями"""
+    with open("data/comments.json", "r", encoding="utf-8") as file:
+        return json.loads(file.read())
+
+def get_bookmarks():
+    """Возвращает закладки"""
+    with open("data/bookmarks.json", "r", encoding="utf-8") as file:
+        return json.loads(file.read())
 
 
 def get_posts_by_user(user_name):
     """Возвращает посты определенного пользователя"""
     try:
         posts = []
-        for post in get_all():
+        for post in get_posts_all():
             if post["poster_name"] == user_name:
                 posts.append(post["content"])
             return posts
@@ -25,16 +33,27 @@ def get_posts_by_user(user_name):
 def get_comments_by_post_id(post_id):
     """Возвращает комментарии определенного поста"""
     try:
+        post = get_post_by_pk(pk)
         comments = []
-        for comment in get_all():
-            if comment["poster_name"] == post_id:
+        for comment in get_comments_all():
+            if comment["post_id"] == post[pk]:
                 comments.append(comment["content"])
             return comments
     except ValueError:
         return "No comments by that post"
 
 
-def get_bookmarks():
-    """Возвращает закладки"""
-    with open("data/bookmarks.json", "r", encoding="utf-8") as file:
+
+
+
+def get_comments():
+    """Возвращает список словарей комментариев"""
+    with open("data/comments.json", "r", encoding="utf-8") as file:
         return json.loads(file.read())
+
+
+def get_post_by_pk(pk):
+    """Возвращает пост по номеру"""
+    for post in get_posts_all():
+        if post['pk'] == pk:
+            return post
